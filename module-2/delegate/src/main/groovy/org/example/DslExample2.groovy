f1235b70942ae7bee6329ad1604f797d28a423c0
A.Polyakov <a.polyakov@unlimint.com>
2024-09-07 10:59:31 +0300

    add examples for lesson (03) 11 delegate

--- module-2/delegate/src/main/groovy/org/example/DslExample2.groovy
+++ module-2/delegate/src/main/groovy/org/example/DslExample2.groovy
@@ -1,17 +1,19 @@
 package org.example
 
+import groovy.transform.ToString
+
 //  user code
 
 def result = email {
     from "example@example.com"
-    to = "example@example.com"
-    subject = 'example@example.com'
-    body =  'example@example.com'
+    to = "to@example.com"
+    subject = 'subject'
+    body =  'Hello World!'
 }
 print result
 
 // library code
-
+@ToString
 class EmailSpec {
     def from(String arg) {}
     String to
@@ -21,7 +23,15 @@ class EmailSpec {
 
 def email(@DelegatesTo(EmailSpec) Closure cl) {
     def email = new EmailSpec()
+    /*
+        delegate
+        owner - владелец, место откуда вызывается кложур (по-умолчанию все методы и св-ва ссылаются на него)
+        this
+     */
     def rehydrate = cl.rehydrate(email, this, this)
+    //def rehydrate = cl.rehydrate(email, email, this)
+    // откуда брать св-ва и методы?
+    rehydrate.resolveStrategy = Closure.DELEGATE_ONLY
     rehydrate()
     email
 }
