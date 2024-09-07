75b602467059daf6c761d9a6b151c1e90a5a1ff1
A.Polyakov <a.polyakov@unlimint.com>
2024-09-07 11:14:55 +0300

    add module-3 structure

---
+++ module-3/build-systems/src/main/groovy/org/example/AntBuilderExamples.groovy
@@ -0,0 +1,5 @@
+package org.example
+
+import groovy.ant.AntBuilder
+
+builder = new AntBuilder()
---
+++ module-3/build-systems/src/main/groovy/org/example/GrapeExample.groovy
@@ -0,0 +1,8 @@
+package org.example
+
+@Grapes([
+        @Grab(group='commons-primitives', module='commons-primitives', version='1.0'),
+        @Grab(group='org.ccil.cowan.tagsoup', module='tagsoup', version='0.9.7')])
+class Example {
+// ...
+}
